685321ffcb2a7546de1183223cb2236b994fd8d0
ElenaWawa <727008@salemkeizer.org>
2026-04-20 00:41:07 -0700

    Update SetupResourceCore.java

--- src/main/java/com/github/relativobr/supreme/setup/SetupResourceCore.java
+++ src/main/java/com/github/relativobr/supreme/setup/SetupResourceCore.java
@@ -30,6 +30,9 @@ public class SetupResourceCore {
     registerCoreFabricator(SupremeCoreBlock.RESOURCE_CORE_GRAVEL,
         CustomCoreRecipe.getRecipe(SupremeCoreBlock.RECIPE_RESOURCE_CORE_GRAVEL));
 
+    registerCoreFabricator(SupremeCoreBlock.RESOURCE_CORE_STAIRS,
+        CustomCoreRecipe.getRecipe(SupremeCoreBlock.RECIPE_RESOURCE_CORE_STAIRS));
+
     registerCoreFabricator(SupremeCoreBlock.RESOURCE_CORE_SAND,
         CustomCoreRecipe.getRecipe(SupremeCoreBlock.RECIPE_RESOURCE_CORE_SAND));
 
